60b7ed45da7439d239934d307cf014aed159bccf
Peter M. Elias <petermelias@gmail.com>
2014-07-16 20:05:56 +0000

    automatic commit by gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -16,40 +16,40 @@ gulp.task('test', function (done) {
 });
 
 gulp.task('ci', ['build', 'test'], function () {
-  return gulp.src('coverage/lcov.info')
+  gulp.src('coverage/lcov.info')
   .pipe(plugins.coveralls());
 });
 
 gulp.task('jshint', function () {
-  return gulp.src('src/*')
-    .pipe(plugins.jshint())
-    .pipe(plugins.jshint.reporter('default'));
+  gulp.src('src/*')
+  .pipe(plugins.jshint())
+  .pipe(plugins.jshint.reporter('default'));
 });
 
 gulp.task('size', ['build'], function () {
-  return gulp.src('dist/gulp-srcdeps.min.js')
-    .pipe(plugins.size());
+  gulp.src('dist/gulp-srcdeps.min.js')
+  .pipe(plugins.size());
 });
 
 gulp.task('commit', function () {
-  return gulp.src('./')
-    .pipe(plugins.git.add())
-    .pipe(plugins.git.commit(yargs.argv.m || 'automatic commit by gulp task'));
+  gulp.src('./')
+  .pipe(plugins.git.add())
+  .pipe(plugins.git.commit(yargs.argv.m || 'automatic commit by gulp task'));
 });
 
 gulp.task('build', function () {
-  return gulp.src('src/*.js')
-    .pipe(plugins.concat('gulp-srcdeps.js'))
-    .pipe(gulp.dest('dist'))
-    .pipe(plugins.uglify())
-    .pipe(plugins.rename('gulp-srcdeps.min.js'))
-    .pipe(gulp.dest('dist'));
+  gulp.src('src/*.js')
+  .pipe(plugins.concat('gulp-srcdeps.js'))
+  .pipe(gulp.dest('dist'))
+  .pipe(plugins.uglify())
+  .pipe(plugins.rename('gulp-srcdeps.min.js'))
+  .pipe(gulp.dest('dist'));
 });
 
 gulp.task('bump', function () {
-  return gulp.src('./package.json')
-    .pipe(plugins.bump({type: yargs.argv.b}))
-    .pipe(gulp.dest('./'));
+  gulp.src('./package.json')
+  .pipe(plugins.bump({type: yargs.argv.b}))
+  .pipe(gulp.dest('./'));
 });
 
 gulp.task('tag', function () {
@@ -59,7 +59,7 @@ gulp.task('tag', function () {
 });
 
 gulp.task('push', function () {
-  return plugins.git.push('origin', 'master').end();
+  plugins.git.push('origin', 'master').end();
 });
 
 gulp.task('publish', function (done) {
