13bf3163d66dfd4c007d5f263ae83c5f77f67895
Peter M. Elias <petermelias@gmail.com>
2014-07-16 22:10:33 +0000

    patch release: 0.0.8

--- gulpfile.js
+++ gulpfile.js
@@ -83,7 +83,9 @@ gulp.task('release', ['bump'], function (done) {
   .pipe(plugins.git.add())
   .pipe(plugins.git.commit(rtype + ' release: ' + pkg.version))
   .on('finish', function () {
-    plugins.git.tag(pkg.version, rtype + ' release ' + pkg.version)
+    plugins.git.tag(pkg.version, rtype + ' release ' + pkg.version, {}, function () {
+      console.log('iddles');
+    })
     .on('end', function () {
       console.log('donesky');
       done();
