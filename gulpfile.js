bc42b0a51bc721318d89292b8aab8240a9734fc5
Peter M. Elias <petermelias@gmail.com>
2014-07-16 21:23:45 +0000

    patch release: 0.0.2

--- gulpfile.js
+++ gulpfile.js
@@ -5,68 +5,91 @@ var gulp = require('gulp'),
     yargs = require('yargs'),
     pkg = require('./package.json');
 
-gulp.task('test', function () {
+gulp.task('test', ['jshint'], function (done) {
   gulp.src(['src/*', 'spec/*'])
   .pipe(plugins.istanbul())
   .on('finish', function () {
     gulp.src(['spec/*'])
     .pipe(plugins.mocha({reporter: 'nyan'}))
     .pipe(plugins.istanbul.writeReports())
-    .on('end', function () {});
+    .on('finish', done);
   });
 });
 
-gulp.task('ci', ['build', 'test'], function () {
+gulp.task('ci', ['build'], function (done) {
   gulp.src('coverage/lcov.info')
-  .pipe(plugins.coveralls());
+  .pipe(plugins.coveralls())
+  .on('finish', done);
 });
 
-gulp.task('jshint', function () {
+gulp.task('jshint', function (done) {
   gulp.src('src/*')
   .pipe(plugins.jshint())
-  .pipe(plugins.jshint.reporter('default'));
+  .pipe(plugins.jshint.reporter('default'))
+  .on('finish', done);
 });
 
-gulp.task('size', ['build'], function () {
+gulp.task('size', ['build'], function (done) {
   gulp.src('dist/*.min.js')
-  .pipe(plugins.size());
+  .pipe(plugins.size())
+  .on('finish', done);
 });
 
-gulp.task('commit', function () {
+gulp.task('commit', function (done) {
   gulp.src('./')
   .pipe(plugins.git.add())
-  .pipe(plugins.git.commit(yargs.argv.m || 'automatic commit by gulp task'));
+  .pipe(plugins.git.commit(yargs.argv.m || 'automatic commit by gulp task'))
+  .on('finish', done);
 });
 
-gulp.task('build', function () {
+gulp.task('build', ['test'], function (done) {
   gulp.src('src/*.js')
   .pipe(plugins.sourcemaps.init())
   .pipe(plugins.concat(pkg.name + '.js'))
   .pipe(plugins.sourcemaps.write('./', {addComment: false}))
-  .pipe(gulp.dest('dist'));
-
-  gulp.src('src/*.js')
-  .pipe(plugins.sourcemaps.init())
-  .pipe(plugins.concat(pkg.name + '.min.js'))
-  .pipe(plugins.uglify())
-  .pipe(plugins.sourcemaps.write('./', {addComment: false}))
-  .pipe(gulp.dest('dist'));
+  .pipe(gulp.dest('dist'))
+  .on('finish', function () {
+    gulp.src('src/*.js')
+    .pipe(plugins.sourcemaps.init())
+    .pipe(plugins.concat(pkg.name + '.min.js'))
+    .pipe(plugins.uglify())
+    .pipe(plugins.sourcemaps.write('./', {addComment: false}))
+    .pipe(gulp.dest('dist'))
+    .on('finish', done);
+  });
 });
 
-gulp.task('bump', function () {
+// MAJOR, MINOR, PATCH, PRERELEASE
+gulp.task('release', function (done) {
+  var rtype = yargs.argv.b || 'patch';
+  
   gulp.src('./package.json')
-  .pipe(plugins.bump({type: yargs.argv.b || 'patch'}))
-  .pipe(gulp.dest('./'));
+  .pipe(plugins.bump({type: rtype}))
+  .pipe(gulp.dest('./'))
+  .on('finish', function () {
+    var semver = require('./package.json').version;
+    gulp.src('./')
+    .pipe(plugins.git.add())
+    .pipe(plugins.git.commit(rtype + ' release: ' + semver))
+    .on('finish', function () {
+      plugins.git.tag(semver, 'release ' + semver, {}, function () {});
+      plugins.git.push('origin', 'master')
+      .on('end', function () {
+        plugins.git.push('origin', 'master', {args: '--tags'})
+        .on('end', done)
+        .end();
+      })
+      .end();
+    });
+  });
 });
 
-gulp.task('tag', function () {
-  var semver = require('./package.json').version;
-  plugins.git.tag(semver, 'Release ' + semver, {}, function () {});
-  plugins.git.push('origin', 'master', {args: '--tags'}).end();
-});
-
-gulp.task('push', function () {
-  plugins.git.push('origin', 'master').end();
+gulp.task('push', ['commit'], function (done) {
+  plugins.git.push('origin', 'master')
+  .on('end', function () {
+    plugins.git.push('origin', 'master', {args: '--tags'})
+    .on('end', done);
+  });
 });
 
 gulp.task('publish', function (done) {
@@ -74,6 +97,5 @@ gulp.task('publish', function (done) {
     .on('close', done);
 });
 
-gulp.task('full', ['test', 'jshint', 'build']);
-gulp.task('ship', ['test', 'jshint', 'build', 'bump', 'commit', 'tag', 'push', 'publish']);
+gulp.task('ship', ['build', 'bump', 'commit', 'tag', 'push', 'publish']);
 gulp.task('save', ['test', 'jshint', 'build', 'commit', 'push']);
